b98b14ee051022256970d331e8b228c231e0050c
Phong Truong <khamtruong1114@gmail.com>
2024-12-13 19:16:25 +0700

    this is setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
-import styled from "styled-components";
-import GlobalStyle from "./styles/GlobalStyle";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-const StyledApp = styled.div`
-  padding: 20px;
-`;
-
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button size="medium" variation="primary">
-                Check in
-              </Button>
-              <Button size="small" variation="secondary">
-                Check in
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests"></Input>
-              <Input type="number" placeholder="Number of guests"></Input>
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
