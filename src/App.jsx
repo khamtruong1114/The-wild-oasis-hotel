cd69fe3e0318f0929e46a09afd433044ecca60bf
Phong Truong <khamtruong1114@gmail.com>
2024-12-13 16:19:32 +0700

    Global Styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
 `;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-`;
-const Input = styled.input`
-  border: 1px solid #ddd;
-  padding: 0.8rem 1.2rem;
-  border-radius: 5px;
-`;
+
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
@@ -26,14 +15,17 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check in</Button>
-      <Button>Check in</Button>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+        <Button>Check in</Button>
 
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
-    </StyledApp>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
   );
 }
 
---
+++ src/ui/Input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  padding: 0.8rem 1.2rem;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+export default Input;
